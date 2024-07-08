6e72152090c9c0035137443048cd0b973faec83a
Seven <ahmadaliev.sd@gmail.com>
2024-07-08 17:02:44 +0900

    update user profile functionality created

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,12 +1,12 @@
 import express from "express";
 import { protectRoute } from "../middleware/protectRoute.js";
-import { getUserProfile, followUnfollowUser, getSuggestedUser } from "../controllers/user.controller.js";
+import { getUserProfile, followUnfollowUser, getSuggestedUser, updateUser } from "../controllers/user.controller.js";
 
 const router = express.Router()
 
 router.get("/profile/:username", protectRoute, getUserProfile)
 router.get("/suggested", protectRoute, getSuggestedUser)
 router.post("/follow/:id", protectRoute, followUnfollowUser)
-// router.post("/update", protectRoute, updateUserProfile)
+router.post("/update", protectRoute, updateUser)
 
 export default router
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,18 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import { v2 as cloudinary } from 'cloudinary';
 import connectMongoDB from './db/connectMongoDB.js';
 import cookieParser from 'cookie-parser';
 import authRoutes from './routes/auth.routes.js'
 import userRoutes from './routes/user.routes.js'
 
 dotenv.config();
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
 
 const app = express();
 const PORT = process.env.PORT || 7777;

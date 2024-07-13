c5b886965f949eaf9d0d079d9d7d03c53af63d7e
Seven <ahmadaliev.sd@gmail.com>
2024-07-13 19:25:09 +0900

    installed required packages for frontend, completed ui for posts, hompage, login, signup, posts etc.

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,22 @@
+import daisyui from "daisyui";
+import daisyUIThemes from "daisyui/src/theming/themes";
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      "light",
+      {
+        black: {
+          ...daisyUIThemes["black"],
+          primary: "rgb(29, 155, 240)",
+          secondary: "rgb(24, 24, 24)",
+        },
+      },
+    ],
+  },
+};

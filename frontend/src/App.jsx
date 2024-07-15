5bab9c28cc726bf28e9a88b84cd5687d15a38b2d
Seven <ahmadaliev.sd@gmail.com>
2024-07-15 21:31:43 +0900

    login, logout connected to backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,8 +8,36 @@ import NotificationPage from "./pages/notification/NotificationPage";
 import ProfilePage from "./pages/profile/ProfilePage";
 
 import {Toaster} from 'react-hot-toast'
+import { useQuery } from "@tanstack/react-query";
+import LoadingSpinner from "./components/common/LoadingSpinner";
 
 function App() {
+  const {data, isLoading, error, isError} = useQuery({
+    queryKey: ['authUser'],
+    queryFn: async() => {
+      try {
+        const res = await fetch("/api/auth/me")
+        const data = await res.json()
+
+        if (!res.ok) {
+          throw new Error(data.error || "Something went wrong")
+        }
+        console.log("authUser is here: ", data)
+        return data;
+      } catch (error) {
+        throw new Error(error)
+      }
+    }
+  })
+
+  if (isLoading) {
+    return (
+      <div className="h-screen flex justify-center items-center" >
+        <LoadingSpinner size="lg"/>
+      </div>
+    )
+  }
+
   return (
     <>
       <div className="flex max-w-6xl mx-auto">

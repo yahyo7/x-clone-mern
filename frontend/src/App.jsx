838e37cff6eaf1f46e6d093b24a55a356396e2bb
Seven <ahmadaliev.sd@gmail.com>
2024-07-16 16:00:11 +0900

    auth user and protected routes implemented using react query

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import SignUpPage from "./pages/auth/signup/SignUpPage";
 import LoginPage from "./pages/auth/login/LoginPage";
 import HomePage from "./pages/home/HomePage";
@@ -7,51 +7,74 @@ import RightPanel from "./components/common/RightPanel";
 import NotificationPage from "./pages/notification/NotificationPage";
 import ProfilePage from "./pages/profile/ProfilePage";
 
-import {Toaster} from 'react-hot-toast'
+import { Toaster } from "react-hot-toast";
 import { useQuery } from "@tanstack/react-query";
 import LoadingSpinner from "./components/common/LoadingSpinner";
 
 function App() {
-  const {data, isLoading, error, isError} = useQuery({
-    queryKey: ['authUser'],
-    queryFn: async() => {
+  const {
+    data: authUser,
+    isLoading,
+    error,
+    isError,
+  } = useQuery({
+    queryKey: ["authUser"],
+    queryFn: async () => {
       try {
-        const res = await fetch("/api/auth/me")
-        const data = await res.json()
+        const res = await fetch("/api/auth/me");
+        const data = await res.json();
+
+        if (data.error) return null;
 
         if (!res.ok) {
-          throw new Error(data.error || "Something went wrong")
+          throw new Error(data.error || "Something went wrong");
         }
-        console.log("authUser is here: ", data)
+        // console.log("authUser is here: ", data);
         return data;
       } catch (error) {
-        throw new Error(error)
+        throw new Error(error);
       }
-    }
-  })
+    },
+    retry: false,
+  });
 
   if (isLoading) {
     return (
-      <div className="h-screen flex justify-center items-center" >
-        <LoadingSpinner size="lg"/>
+      <div className="h-screen flex justify-center items-center">
+        <LoadingSpinner size="lg" />
       </div>
-    )
+    );
   }
 
   return (
     <>
       <div className="flex max-w-6xl mx-auto">
         {/* Common components */}
-        <Sidebar/>
+        { authUser && <Sidebar />}
         <Routes>
-          <Route path="/" element={<HomePage/>}/>
-          <Route path="/login" element={<LoginPage/>}/>
-          <Route path="/signup" element={<SignUpPage/>}/>
-          <Route path="/notifications" element={<NotificationPage/>}/>
-          <Route path="/profile/:username" element={<ProfilePage/>}/>
+          <Route
+            path="/"
+            element={authUser ? <HomePage /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/login"
+            element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/signup"
+            element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/notifications"
+            element={authUser ? <NotificationPage /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/profile/:username"
+            element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
+          />
         </Routes>
-        <RightPanel/>
-        <Toaster/>
+        {authUser && <RightPanel />}
+        <Toaster />
       </div>
     </>
   );
--- frontend/src/pages/auth/login/LoginPage.jsx
+++ frontend/src/pages/auth/login/LoginPage.jsx
@@ -5,7 +5,7 @@ import XSvg from "../../../components/svgs/X";
 
 import { MdOutlineMail } from "react-icons/md";
 import { MdPassword } from "react-icons/md";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 
 const LoginPage = () => {
@@ -13,6 +13,8 @@ const LoginPage = () => {
     username: "",
     password: "",
   });
+
+  const queryClient = useQueryClient();
 
   const {
     mutate: loginMutation,
@@ -41,6 +43,7 @@ const LoginPage = () => {
     },
     onSuccess: () => {
       toast.success("Login Succesfully");
+      queryClient.invalidateQueries({ queryKey: ["authUser"] });
     },
   });
 

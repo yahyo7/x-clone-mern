45d30f25dfd61aac56a5b4f07460ada3d40b8c88
Seven <ahmadaliev.sd@gmail.com>
2024-07-19 16:37:12 +0900

    payload limit updated for img upload

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ cloudinary.config({
 const app = express();
 const PORT = process.env.PORT || 7777;
 
-app.use(express.json()); // to parse req,body
+app.use(express.json({limit: "5mb"})); // to parse req,body
 app.use(express.urlencoded({extended: true})) // to parse from data
 
 app.use(cookieParser()); // help to get cookies

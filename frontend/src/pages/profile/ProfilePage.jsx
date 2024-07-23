b57a8c265830e6097b687420660c6782022c8aed
Seven <ahmadaliev.sd@gmail.com>
2024-07-23 15:45:09 +0900

    update user profile info and images functionality completed

--- frontend/src/pages/profile/ProfilePage.jsx
+++ frontend/src/pages/profile/ProfilePage.jsx
@@ -4,6 +4,7 @@ import { Link, useParams } from "react-router-dom";
 import Posts from "../../components/common/Posts";
 import ProfileHeaderSkeleton from "../../components/skeletons/ProfileHeaderSkeleton";
 import EditProfileModal from "./EditProfileModal.jsx";
+import useFollow from "../../hooks/useFollow";
 
 import { POSTS } from "../../utils/db/dummy";
 
@@ -11,8 +12,9 @@ import { FaArrowLeft } from "react-icons/fa6";
 import { IoCalendarOutline } from "react-icons/io5";
 import { FaLink } from "react-icons/fa";
 import { MdEdit } from "react-icons/md";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient} from "@tanstack/react-query";
 import { formatMemberSinceDate } from "../../utils/date/index.js";
+import toast from "react-hot-toast";
 
 const ProfilePage = () => {
   const [coverImg, setCoverImg] = useState(null);
@@ -22,8 +24,11 @@ const ProfilePage = () => {
   const coverImgRef = useRef(null);
   const profileImgRef = useRef(null);
 
+  const queryClient = useQueryClient();
+  const {data: authUser} = useQuery({queryKey: ["authUser"]})
   const {username} = useParams();
   
+  const {follow, isPending} = useFollow()
 
   const { data: user, isLoading, refetch, isRefetching } = useQuery({
     queryKey: ["userProfile"],
@@ -41,7 +46,41 @@ const ProfilePage = () => {
     },
   });
 
-  const isMyProfile = true
+  const {mutate: updateProfile, isPending:isUpdatingProfile} = useMutation({
+    mutationFn: async () => {
+      try {
+        const res = await fetch(`/api/users/update`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            coverImg, profileImg
+          }),
+        });
+        const data = await res.json();
+        if (!res.ok) {
+          throw new Error(data.error || "Something went wrong");
+        }
+        return data;
+      } catch (error) {
+        throw new Error(error.message);
+      }
+    },
+    onSuccess: () => {
+      toast.success("Profile updated successfully")
+      Promise.all([
+        queryClient.invalidateQueries({queryKey: ["authUser"]}),
+        queryClient.invalidateQueries({queryKey: ["userProfile"]}),
+      ])
+    },
+    onError: (error) => {
+      toast.error(error.message)
+    }
+  })
+
+  const isMyProfile = authUser._id === user?._id
+  const amIFollowing = authUser?.following.includes(user?._id)
 
   const memberSinceDate = formatMemberSinceDate(user?.createdAt);
 
@@ -133,21 +172,23 @@ const ProfilePage = () => {
                 </div>
               </div>
               <div className="flex justify-end px-4 mt-5">
-                {isMyProfile && <EditProfileModal />}
+                {isMyProfile && <EditProfileModal authUser={authUser} />}
                 {!isMyProfile && (
                   <button
                     className="btn btn-outline rounded-full btn-sm"
-                    onClick={() => alert("Followed successfully")}
+                    onClick={() => follow(user?._id)}
                   >
-                    Follow
+                    {isPending && "Loading..." }
+                    {!isPending && amIFollowing && "Unfollow" }
+                    {!isPending && !amIFollowing && "Follow" }
                   </button>
                 )}
                 {(coverImg || profileImg) && (
                   <button
                     className="btn btn-primary rounded-full btn-sm text-white px-4 ml-2"
-                    onClick={() => alert("Profile updated successfully")}
+                    onClick={() => updateProfile()}
                   >
-                    Update
+                    {isUpdatingProfile ? "Updating..." : "Update"}
                   </button>
                 )}
               </div>
@@ -167,12 +208,12 @@ const ProfilePage = () => {
                       <>
                         <FaLink className="w-3 h-3 text-slate-500" />
                         <a
-                          href="https://youtube.com/@asaprogrammer_"
+                          href={user?.link}
                           target="_blank"
                           rel="noreferrer"
                           className="text-sm text-blue-500 hover:underline"
                         >
-                          youtube.com/@asaprogrammer_
+                          {user?.link}
                         </a>
                       </>
                     </div>
